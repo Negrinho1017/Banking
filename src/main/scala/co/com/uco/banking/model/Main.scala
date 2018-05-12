ea52d43d49a0466e5c5d1fbde4716cf885b84c47
juanftech <juan.restrepo@techandsolve.com>
2018-05-12 16:07:56 -0500

    Actualizacion de version de slick y conexion a bases de datos para crear tablas

--- src/main/scala/co/com/uco/banking/model/Main.scala
+++ src/main/scala/co/com/uco/banking/model/Main.scala
@@ -1,15 +1,18 @@
 
 package co.com.uco.banking.model
 
-import scala.slick.driver.PostgresDriver.simple._
+import slick.jdbc.PostgresProfile.api._
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 object Main {
 
+  case class Client(id: Int, username: String)
   // this is a class that represents the table I've created in the database
-  class Client(tag: Tag) extends Table[(Int, String)](tag, "clients") {
+  class Clients(tag: Tag) extends Table[Client](tag, "clients") {
     def id = column[Int]("id")
     def username = column[String]("username")
-    def * = (id, username)
+    def * = (id, username) <> (Client.tupled, Client.unapply)
   }
 
   def main(args: Array[String]): Unit = {
@@ -20,20 +23,22 @@ object Main {
     // and password is "postgres"
     val connectionUrl = "jdbc:postgresql://localhost/banking?user=postgres&password=tech"
 
-    Database.forURL(connectionUrl, driver = "org.postgresql.Driver") withSession {
-      implicit session =>
-        val clients = TableQuery[Client]
+    def Clients = TableQuery[Clients]
+    val db = Database.forURL(connectionUrl, driver = "org.postgresql.Driver")
 
-        // SELECT * FROM clients
-        clients.list foreach { row =>
-          println("user with id " + row._1 + " has username " + row._2)
-        }
-
-        // SELECT * FROM clients WHERE username='john'
-        clients.filter(_.username === "john").list foreach { row =>
-          println("user whose name is 'john' has id " + row._1)
-        }
-
+    val setup = DBIO.seq(
+      Clients.schema.create,
+      Clients += Client(3,"hola")
+    )
+    def create = Future {
+      db.run(setup)
     }
+    create
+    def query = Future {
+      db.run(Clients.result).map(_.foreach{
+        case e => println(s"clients => ${e.id}")
+      })
+    }
+    query
   }
 }
