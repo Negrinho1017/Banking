155f40b47fb10e2c624abe5534e386af2e9bb020
william valencia <williamvalencia@seven4n.com>
2018-05-19 13:51:07 -0500

    Add route for proof

--- app/co/com/banking/presentation/controllers/UserController.scala
+++ app/co/com/banking/presentation/controllers/UserController.scala
@@ -1,7 +1,9 @@
 package co.com.banking.presentation.controllers
 
 import javax.inject._
+
 import play.api._
+import play.api.libs.json.Json
 import play.api.mvc._
 
 @Singleton
@@ -16,5 +18,9 @@ class UserController @Inject()(cc: ControllerComponents) extends AbstractControl
     */
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
+  }
+
+  def proofJson() = Action { implicit request: Request[AnyContent] =>
+    Ok(Json.toJson("Welcome to project banking"))
   }
 }
