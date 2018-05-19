4eabb792020c41d38bba13ae9c99e9a43a8fe8c9
william valencia <williamvalencia@seven4n.com>
2018-05-19 14:53:30 -0500

    Delete repeat class

--- app/co/com/banking/domain/account/State.scala
+++
@@ -1,6 +0,0 @@
-package co.com.uco.banking.model.account
-
-sealed trait State
-
-case object Active extends State
-case object Inactive extends State
--- app/co/com/banking/domain/account/TypeAccount.scala
+++
@@ -1,6 +0,0 @@
-package co.com.uco.banking.model.account
-
-trait TypeAccount
-
-case object Bank extends TypeAccount
-case object Personal extends TypeAccount
