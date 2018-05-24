d452c1bf98816712381d652f7f1eb747e9e7792f
Negrinho1017 <camilo17746@gmail.com>
2018-05-24 10:58:38 -0500

    Object TransferValue estaba mal

--- app/co/com/banking/model/transfer/TransferValue.scala
+++ app/co/com/banking/model/transfer/TransferValue.scala
@@ -5,7 +5,7 @@ import co.com.uco.banking.model.exceptions.TransferValueException
 class TransferValue(value: BigDecimal) {
 
 }
-object LendingValue{
+object TransferValue{
   def apply(transferValue: BigDecimal): Either[TransferValueException, TransferValue] = transferValue match {
     case a if a >= 0 => Right(new TransferValue(a))
     case _ => Left(TransferValueException(description = "The Transfer value is negative"))
