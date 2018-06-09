f7dd52ffe068eb1212c0a0b8f8b7871a57d474a6
William Fernando Valencia Otalvaro <williamvalencia@seven4n.com>
2018-06-09 08:30:49 -0500

    Se ajusta accountRepository

--- app/co/com/banking/domain/services/ClientService.scala
+++ app/co/com/banking/domain/services/ClientService.scala
@@ -1,8 +1,17 @@
 package co.com.banking.domain.services
 
-class ClientService{
+import co.com.banking.infrastructure.builders.AccountBuilder
+import co.com.banking.infrastructure.persistence.dao.{AccountDAO, BankMovementsDAO}
+import co.com.banking.infrastructure.persistence.repository.AccountRepository
+import javax.inject.Inject
 
-  def getClient(typeId:String, numId:String) ={
+import scala.concurrent.ExecutionContext
+
+class ClientService @Inject()(
+  accountReposity: AccountRepository){
+
+  def getClient(typeId:String, numId:String) = {
+
     //consulta al repository y me devuelve la cuenta
   }
 
--- app/co/com/banking/infrastructure/persistence/repository/AccountRepository.scala
+++ app/co/com/banking/infrastructure/persistence/repository/AccountRepository.scala
@@ -1,29 +1,18 @@
 package co.com.banking.infrastructure.persistence.repository
 
 import co.com.banking.domain.entities.account.Account
-import co.com.banking.domain.entities.client.Client
 import co.com.banking.infrastructure.builders.AccountBuilder
 import co.com.banking.infrastructure.persistence.dao.{AccountDAO, BankMovementsDAO}
 import co.com.banking.infrastructure.persistence.dto.{AccountDto, BankMovementsDto}
-import domain.exceptions.ModelErrors
 import javax.inject.Inject
 
 import scala.concurrent.{ExecutionContext, Future}
-import scala.util.Try
 
 class AccountRepository @Inject()(
-accountDAO: AccountDAO,
-bankMovementsDAO: BankMovementsDAO,
-accountBuilder: AccountBuilder
-)(implicit executionContext: ExecutionContext){
-
-
-  def saveConsigAccount(account: Account, client: Client): Account={
-    //llamamamos los dao y persistimos la información
-
-    //devuelve la cuenta con los datos actualizados
-    account:Account
-  }
+  accountDAO: AccountDAO,
+  bankMovementsDAO: BankMovementsDAO,
+  accountBuilder: AccountBuilder
+  )(implicit executionContext: ExecutionContext){
 
   def getAccountById(accountId:String)= {
     for {
@@ -33,10 +22,14 @@ accountBuilder: AccountBuilder
     }
   }
 
-  //recibimos los datos necesarios para guardar el movimiento
-  //def saveMovement(accountOrigin: Account, accountDestination: Account, typeMov: String, ammount:Double) ={
-    //val movementDTO = BankMovementsDto.apply("djfsf343", typeMov, accountOrigin, accountDestination, ammount)
-    //bankMovementsDAO.insertNewMovement()
- // }
+  def saveMovement(accountOrigin: Account, accountDestination: Account, typeMov: String, value:BigDecimal) ={
+    val movementDTO = BankMovementsDto.apply(
+      "1",
+      Option(typeMov),
+      Option(accountOrigin.accountNumber.number.toString),
+      Option(accountDestination.accountNumber.number.toString),
+      Option(value.doubleValue()))
+    bankMovementsDAO.insertNewMovement(movementDTO)
+  }
 
 }
